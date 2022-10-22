79e3b61592028b57c8ab93cc24b030cd356866c3
gabe <playwigs@gmail.com>
2022-10-21 19:30:53 -0600

    Create User model.

--- models.py
+++ models.py
@@ -10,3 +10,15 @@ def connect_db(app):
 
     with app.app_context():
         db.create_all()
+
+
+class User(db.Model):
+    """Stores user info."""
+
+    __tablename__ = "users"
+
+    username = db.Column(db.Text(20), primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text(50), nullable=False, unique=True)
+    first_name = db.Column(db.Text(30), nullable=False)
+    last_name = db.Column(db.Text(30), nullable=False)
