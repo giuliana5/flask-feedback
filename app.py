2d1dc243b3bb1cbae887604c4fcc324df88758c3
gabe <playwigs@gmail.com>
2022-10-21 19:30:37 -0600

    Import & connect to db.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret'
 
 toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
